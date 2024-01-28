90d2af642b2c089b07319af63a48c191f622b641
Tathya Dixit <dixit.tathya2003@gmail.com>
2024-01-28 11:45:42 +0530

    browsing-feature

--- item/views.py
+++ item/views.py
@@ -2,17 +2,24 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from .forms import AddItemForm
-from item.models import Item
+from item.models import Item,Category
 
 
 def browse(request):
     items = Item.objects.filter(is_sold=False)
+    categories = Category.objects.all()
     query = request.GET.get('query','')
+    category_id = request.GET.get('category',0)
+    items = Item.objects.filter(is_sold = False)
+    if category_id:
+        items = items.filter(category_id=category_id)
     if query :
-        items = Item.objects.filter(Q(name__icontains = query) | Q(description__icontains=query))
+        items = items.filter(Q(name__icontains = query) | Q(description__icontains=query))
     context = {
         'items' : items,
         'query' : query,
+        'categories' : categories,
+        'category_id' : int(category_id)
     }
     return render(request,'items/browse.html',context)
 
